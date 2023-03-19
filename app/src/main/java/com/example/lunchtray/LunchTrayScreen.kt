f76c66722e43b82203b6040481ceb3453521e64a
илья бугай <bugai.samara@yandex.ru>
2023-03-19 21:25:57 +0400

    Создал котнреллер навигации Инициализировал запись обратного стека и имя текущего экрана. Пытался добавить диалоговое окно на последний экран,но по ка что не получается

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -16,17 +16,23 @@
 package com.example.lunchtray
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.lunchtray.ui.OrderViewModel
+import com.example.lunchtray.datasource.DataSource
+import com.example.lunchtray.ui.*
 
 // TODO: Screen enum
 //класс для перечисления маршрутов
@@ -62,8 +68,119 @@ fun LunchTrayApp(modifier: Modifier = Modifier) {
             modifier = modifier.padding(innerPadding)
         ) {
             composable(MenuScreen.Start.name) {
+                StartOrderScreen(onStartOrderButtonClicked = {
+                    navController.navigate(MenuScreen.Entree.name)
+                }
+                )
+            }
+            composable(MenuScreen.Entree.name) {
+                EntreeMenuScreen(
+                    options = DataSource.entreeMenuItems,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(MenuScreen.Side.name)
 
+                    },
+                    onSelectionChanged = { viewModel.updateEntree(it) }
+                )
+
+            }
+            composable(MenuScreen.Side.name) {
+                SideDishMenuScreen(
+                    options = DataSource.sideDishMenuItems,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(MenuScreen.Accompaniment.name)
+
+                    },
+                    onSelectionChanged = { viewModel.updateSideDish(it) }
+                )
+
+            }
+            composable(MenuScreen.Accompaniment.name) {
+                AccompanimentMenuScreen(
+                    options = DataSource.accompanimentMenuItems,
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(MenuScreen.Checkout.name)
+                    },
+                    onSelectionChanged = { viewModel.updateAccompaniment(it) }
+                )
+            }
+            composable(MenuScreen.Checkout.name) {
+                CheckoutScreen(
+                    orderUiState = uiState,
+                    onNextButtonClicked = { },
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(
+                            viewModel,
+                            navController
+                        )
+                    })
             }
         }
     }
+
+
 }
+
+//метод для возвращение на начальный экран
+private fun cancelOrderAndNavigateToStart(
+    viewModel: OrderViewModel,
+    navController: NavHostController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(MenuScreen.Start.name, inclusive = false)
+}
+
+
+/*
+@Composable
+private fun ScreenDialog(
+    orderPrice: Double,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val activity = (LocalContext.current) as MainActivity
+
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        title = { Text(text = stringResource(id = R.string.titleDialog)) },
+        text = { Text(text = stringResource(id = R.string.infoDialog, orderPrice)) },
+        modifier = modifier,
+        confirmButton = {
+            TextButton(onClick = { onClick() }) {
+                Text(text = stringResource(id = R.string.BottomDialog))
+
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    activity.finish()
+                }
+            ) {
+                Text(text = "Exit")
+            }
+        },
+
+
+    )
+
+}
+ */
